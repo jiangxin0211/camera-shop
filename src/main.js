0c623e24a79aff59f1ca3a5ff6f2970c4c9a0a88
jiangxin <jiangxin0211@163.com>
2019-03-03 23:56:13 +0800

    注册

--- src/main.js
+++ src/main.js
@@ -14,9 +14,9 @@ import '@/assets/js/rem.js'
 //
 // Vue.use(Vant)
 
-import { Button, Icon, NavBar,Tabbar,TabbarItem, Swipe, SwipeItem, Lazyload } from 'vant';
+import { Toast, CellGroup, Field, Tab, Tabs, Button, Icon, NavBar,Tabbar,TabbarItem, Swipe, SwipeItem, Lazyload } from 'vant';
 
-Vue.use(Button).use(Icon).use(NavBar).use(Tabbar).use(TabbarItem).use(Swipe).use(SwipeItem).use(Lazyload);
+Vue.use(Toast).use(CellGroup).use(Field).use(Tab).use(Tabs).use(Button).use(Icon).use(NavBar).use(Tabbar).use(TabbarItem).use(Swipe).use(SwipeItem).use(Lazyload);
 
 import '@/mock/mock.js'
 
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,9 +1,63 @@
 <template>
   <div>
-    我的
+    <van-tabs>
+      <van-tab title="登录">
+        <van-cell-group>
+          <van-field label="用户名" placeholder="请输入用户名" required clearable v-model="loginUsername"></van-field>
+          <van-field label="密码" type="password" placeholder="请输入密码" required clearable v-model="loginPassword"></van-field>
+        </van-cell-group>
+        <div>
+          <van-button type="primary" size="large">登录</van-button>
+        </div>
+      </van-tab>
+      <van-tab title="注册">
+        <van-cell-group>
+          <van-field label="用户名" placeholder="请输入用户名" required clearable v-model="registUsername"></van-field>
+          <van-field label="密码" type="password" placeholder="请输入密码" required clearable v-model="registPassword"></van-field>
+        </van-cell-group>
+        <div>
+          <van-button @click="registHandler" type="primary" size="large">注册</van-button>
+        </div>
+      </van-tab>
+    </van-tabs>
   </div>
 </template>
 
 <script>
-
+import axios from 'axios';
+import url from '@/service.config.js';
+export default{
+  data () {
+    return {
+      loginUsername: '',
+      loginPassword: '',
+      registUsername:  '',
+      registPassword: ''
+    }
+  },
+  methods: {
+    // 注册
+    registHandler () {
+      axios({
+        url: url.registUser,
+        method: 'post',
+        data: {
+          userName: this.registUsername,
+          password: this.registPassword
+        }
+      }).then(res => {
+          if (res.data.code === 200) {
+              this.$toast.success('注册成功')
+              this.registUsername = this.registPassword = '';
+          } else {
+              this.$toast.fail('注册失败')
+          }
+          console.log(res);
+      }).catch(err => {
+          console.log(err);
+          this.$toast.fail('注册失败')
+      })
+    }
+  }
+}
 </script>
